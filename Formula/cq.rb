8f7be02551a5caa5cb42b4644b3d67036b8f9ae1
Markus <mark@saiko.tech>
2023-10-18 18:26:06 +0100

    update cq to latest version

--- Formula/cq.rb
+++ Formula/cq.rb
@@ -2,19 +2,19 @@ class Cq < Formula
 
   desc "Clojure Command-line Data Processor for JSON, YAML, EDN, XML and more"
   homepage "https://github.com/markus-wa/cq"
-  version "2022.11.14-14.16.12"
+  version "2023.10.18-18.12"
 
   @@binary = ""
 
   if OS.linux?
     @@binary = "cq-native-linux"
-    sha256 "de271d5bc44f7b256196ef9e51350a6dbcb6f4c715cbe9201b8e41d0a480d022"
+    sha256 "d61bba55bb974dc3b1d845607fa4864a941487ecae967ff5ba0ae702dae16cd8"
   else
     @@binary = "cq-native-macos"
-    sha256 "0215e90b9125025a066c70e2dd141231ca5183359c3ac3afdac9829c395b73c6"
+    sha256 "b6dc057299b02fe39ecb63b1ebe4044ae66370b6150abf15ba2561a43fcc9323"
   end
 
-  url "https://github.com/markus-wa/cq/releases/download/2022.11.14-14.16.12/#{@@binary}"
+  url "https://github.com/markus-wa/cq/releases/download/2023.10.18-18.12/#{@@binary}"
 
   def install
     bin.install @@binary => "cq"
